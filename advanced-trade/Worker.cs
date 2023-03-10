625bd4f9f89f3ef25d8daf2ba23bec17c115f29b
danroche10 <danielroche10@gmail.com>
2023-03-10 11:34:04 +0000

    feature: correctly capturing indicator price changes

--- advanced-trade/Worker.cs
+++ advanced-trade/Worker.cs
@@ -3,6 +3,9 @@ namespace trade_execution
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private int _executionCount;
+        private List<float> _indicatorCoinTodayAndYesterday = new List<float>();
+        private readonly int oneHourInMilliseconds = 3600000;
 
         public Worker(ILogger<Worker> logger)
         {
@@ -13,49 +16,57 @@ namespace trade_execution
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                float indicatorCoinPriceChange = await getIndicatorCoinPriceChange();
+                _executionCount++;
+                
+                await updatePriceComparisonList();
+                
+                if (_executionCount < 3)
+                {
+                    continue;
+                }
 
+                float indicatorCoinPriceChange = getIndicatorCoinPriceChange();
                 CoinbaseOrderDetails coinbaseOrderDetails = new CoinbaseOrderDetails();
                 ExecuteCoinbaseTrade executeCoinbaseTrade = new ExecuteCoinbaseTrade(coinbaseOrderDetails);
 
                 executeTrade(executeCoinbaseTrade, indicatorCoinPriceChange);
 
-                await Task.Delay(60000, stoppingToken);
+                await Task.Delay(oneHourInMilliseconds, stoppingToken);
             }
         }
 
-        private static async Task<float> getIndicatorCoinPriceChange()
+        private async Task updatePriceComparisonList()
         {
-            List<float> indicatorCoinTodayAndYesterday = new List<float>();
-
-            // hard code fake price from yesterday and day before
-            indicatorCoinTodayAndYesterday.Add(19500);
-            indicatorCoinTodayAndYesterday.Add(20000);
-
             string indicatorCoinId = "bitcoin";
             string indicatorCoinPriceRequestUrl = $"https://api.coingecko.com/api/v3/simple/price?ids={indicatorCoinId}&vs_currencies=gbp";
 
-            // remove fake price from day before yesterday
-            indicatorCoinTodayAndYesterday.RemoveAt(0);
-
+            if (_executionCount > 2)
+            {
+                _indicatorCoinTodayAndYesterday.RemoveAt(0);
+            }
+            
             CoinPrice coinPriceData = new CoinPrice();
             float indicatorCoinPriceToday = await coinPriceData.getIndicatorPriceToday(indicatorCoinPriceRequestUrl);
-            indicatorCoinTodayAndYesterday.Add(indicatorCoinPriceToday);
+            _indicatorCoinTodayAndYesterday.Add(indicatorCoinPriceToday);
+            Console.WriteLine($"indicatorCoinPriceToday: {_indicatorCoinTodayAndYesterday.FirstOrDefault()}, indicatorCoinPriceToday: {_indicatorCoinTodayAndYesterday.LastOrDefault()}");
+        }
 
-            float indicatorCoinPriceChange = CoinPrice.getIndicatorCoinPriceChange(indicatorCoinTodayAndYesterday);
-
+        private float getIndicatorCoinPriceChange()
+        {
+            float indicatorCoinPriceChange = CoinPrice.getIndicatorCoinPriceChange(_indicatorCoinTodayAndYesterday);
+            Console.WriteLine($"indicator price change: {indicatorCoinPriceChange}");
             return indicatorCoinPriceChange;
         }
 
         private static void executeTrade(ExecuteCoinbaseTrade executeCoinbaseTrade, float indicatorCoinPriceChange)
         {
-            if (indicatorCoinPriceChange > 1.02)
+            if (indicatorCoinPriceChange > 1.005)
             {
                 Console.WriteLine("Buy order executed");
                 //executeCoinbaseTrade.PlaceBuyOrder();
             }
 
-            if (indicatorCoinPriceChange < 0.98)
+            if (indicatorCoinPriceChange < 0.995)
             {
                 Console.WriteLine("Sell order executed");
                 //executeCoinbaseTrade.PlaceSellOrder();
